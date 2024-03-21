855925216b7e99222d2ce6a26f5a39185afb2104
Fadel Drissi Toubbali <fadel.toubbali@coolbet.com>
2024-03-22 01:45:58 +0200

    feat: allow concurrent connections

--- app/main.js
+++ app/main.js
@@ -1,15 +1,10 @@
 const net = require('net');
-const formatResponse = require('./formatResponse');
 const requestParser = require('./requestParser');
 const routes = require('./routes');
-
-const TWO_HUNDRED_RESPONSE = 'HTTP/1.1 200 OK';
-const FOUR_OH_FOU_RESPONSE = 'HTTP/1.1 404 Not Found';
 
 const server = net.createServer((socket) => {
   socket.on('close', () => {
     socket.end();
-    // server.close();
   });
 
   socket.on('data', (data) => {
