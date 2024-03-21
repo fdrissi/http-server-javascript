6a1ac7e5576ff34d9eb3038e9dee5b5939d7d22c
Fadel Drissi Toubbali <fadel.toubbali@coolbet.com>
2024-03-21 19:17:58 +0200

    feat: resoponse with 200

--- app/main.js
+++ app/main.js
@@ -9,6 +9,10 @@ const server = net.createServer((socket) => {
     socket.end();
     server.close();
   });
+
+  socket.on('data', () => {
+    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+  });
 });
 
 server.listen(4221, 'localhost');
